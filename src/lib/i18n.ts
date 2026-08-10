022070235f95d1144bb9ad374c3b527da1a4a526
Mas Dar <terateinka@gmail.com>
2026-08-10 18:23:36 +0700

    Update i18n.ts

--- src/lib/i18n.ts
+++ src/lib/i18n.ts
@@ -24,7 +24,7 @@ export function isLocale(value: string): value is Locale {
 }
 
 export function getLocaleFromPath(pathname: string): Locale {
-  return pathname === '/en' || pathname.startsWith('/en/') ? 'en' : 'de';
+  return pathname === '/en' || pathname.startsWith('/en/') ? 'en' : 'id';
 }
 
 export function stripLocaleFromPath(pathname: string) {
